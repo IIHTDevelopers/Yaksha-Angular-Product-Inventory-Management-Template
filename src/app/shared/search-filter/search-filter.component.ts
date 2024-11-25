c6974568d66c66feca27e9af45bc91dc2faa297a
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-11-25 19:44:31 +0530

    Update search-filter.component.ts

--- src/app/shared/search-filter/search-filter.component.ts
+++ src/app/shared/search-filter/search-filter.component.ts
@@ -3,7 +3,7 @@ import { Component, EventEmitter, Output } from '@angular/core';
 @Component({
   selector: 'app-search-filter',
   templateUrl: './search-filter.component.html',
-  styleUrls: ['./search-filter.component.scss']
+  styleUrls: ['./search-filter.component.css']
 })
 export class SearchFilterComponent {
   @Output() search = new EventEmitter<string>();
